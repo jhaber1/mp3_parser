e36418d1e3a0cb198f09e3760b6aef670eb1d25e
Jake Haber <singjsong@gmail.com>
2016-07-30 12:35:56 -0400

    Progress
    Parse Angel Sword mp3 file, id3v2 format
    Output song title

--- lib/unzipper.ex
+++ lib/unzipper.ex
@@ -1,8 +1,10 @@
 require IEx;
 
 defmodule Unzipper do
+  @folder '/Users/jhaber/Music'
+
   def start do
-    :fs.start_link(:watcher, "/Users/jhaber/Music")
+    :fs.start_link(:watcher, @folder)
     :fs.subscribe(:watcher)
     watch   
   end
@@ -11,20 +13,23 @@ defmodule Unzipper do
   defp watch do
     receive do
       {_watcher_process, {:fs, :file_event}, {changed_file, type}} ->
-        if to_string(changed_file) =~ ~r/\.(zip|rar|7z)$/i do
-          IEx.pry
+        if to_string(changed_file) =~ ~R/\.(zip|rar|7z)$/i do
+          results = :zip.unzip(changed_file, cwd: './tmp')
+          mp3s = Path.wildcard("./tmp/*.mp3")
 
+          # Load first mp3 to parse details
+          binary = File.read!(List.first(mp3s))
 
-
-          
-          :zip.unzip(changed_file)
+          # ID3v2 parsing
+          # First 23 bytes are irrelvant ID3v2 header stuff
+          << _ :: binary-size(23),
+            title :: binary-size(22), _ :: binary >> = binary
+          IO.puts(title)
         else
           watch
         end
 
-        
-        #IO.puts("#{changed_file} was updated with #{Enum.join(type,", ")}")
         watch
       end
-  end        
+  end
 end
